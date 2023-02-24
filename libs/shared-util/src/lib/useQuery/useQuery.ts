41286f3507e70f1954ad46bd5d00da0f72a67d93
omkar-joshi <omkar.joshi@velotio.com>
2023-02-24 17:58:55 +0530

    Updated base url

--- libs/shared-util/src/lib/useQuery/useQuery.ts
+++ libs/shared-util/src/lib/useQuery/useQuery.ts
@@ -1,7 +1,9 @@
 import { useFetch } from 'use-http';
 
+const NX_API_BASE_URL = 'https://63f5fee4ab76703b15b60449.mockapi.io/api/v1';
+
 export const useQuery = (endpoint: string) => {
-  const serverUrl = `${process.env.NX_API_BASE_URL}${endpoint}`;
+  const serverUrl = `${NX_API_BASE_URL}${endpoint}`;
   const { data, loading } = useFetch(serverUrl, {}, []);
 
   return { data, loading };
